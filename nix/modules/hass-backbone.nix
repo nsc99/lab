b410760312fddbff0e9346735a7ec7874ef4912d
nils <nils@schweigert.gg>
2026-05-10 20:18:47 +0200

    feature: added z2m and mosquitto

---
+++ nix/modules/hass-backbone.nix
@@ -0,0 +1,110 @@
+{ config, ... }:
+
+{
+  networking.firewall.allowedTCPPorts = [
+    8099 # zigbee2mqtt
+    1883 # mosquitto
+  ];
+
+  sops.secrets = {
+    "zigbee2mqtt-secret.yaml" = {
+      owner = config.users.users.zigbee2mqtt.name;
+      group = config.users.users.zigbee2mqtt.group;
+      sopsFile = ../secrets/zigbee2mqtt.yaml;
+      path = "${config.services.zigbee2mqtt.dataDir}/secret.yaml";
+    };
+
+    "zigbee2mqtt-devices.yaml" = {
+      owner = config.users.users.zigbee2mqtt.name;
+      group = config.users.users.zigbee2mqtt.group;
+      sopsFile = ../secrets/z2m-devices.enc;
+      format = "binary";
+      path = "${config.services.zigbee2mqtt.dataDir}/devices.yaml";
+    };
+
+    "mosquitto-zigbee2mqtt-password" = {
+      sopsFile = ../secrets/mosquitto.yaml;
+      owner = "mosquitto";
+      group = "mosquitto";
+    };
+
+    "mosquitto-homeassistant-password" = {
+      sopsFile = ../secrets/mosquitto.yaml;
+      owner = "mosquitto";
+      group = "mosquitto";
+    };
+  };
+
+  services.zigbee2mqtt.enable = true;
+  services.zigbee2mqtt = {
+    dataDir = "/var/lib/zigbee2mqtt";
+
+    settings = {
+      serial = {
+        port = "/dev/ttyUSB0";
+        adapter = "ember";
+        baudrate = 115200;
+        rtscts = false;
+      };
+
+      mqtt = {
+        base_topic = "zigbee2mqtt";
+        server = "!secret mqtt_server";
+        user = "!secret mqtt_user";
+        password = "!secret mqtt_password";
+      };
+
+      advanced = {
+        log_level = "info";
+        channel = 11;
+        network_key = "!secret network_key";
+        pan_id = 52343;
+        ext_pan_id = [
+          75
+          80
+          193
+          40
+          147
+          11
+          28
+          1
+        ];
+      };
+
+      frontend = {
+        enabled = true;
+        port = 8099;
+      };
+
+      homeassistant.enabled = true;
+
+      devices = "devices.yaml";
+    };
+
+  };
+
+  services.mosquitto.enable = true;
+  services.mosquitto = {
+    listeners = [
+      {
+        address = "0.0.0.0";
+        port = 1883;
+
+        users = {
+          z2m = {
+            passwordFile = config.sops.secrets."mosquitto-zigbee2mqtt-password".path;
+            acl = [
+              "readwrite zigbee2mqtt/#"
+              "readwrite homeassistant/#"
+            ];
+          };
+
+          hass = {
+            passwordFile = config.sops.secrets."mosquitto-homeassistant-password".path;
+            acl = [ "readwrite #" ];
+          };
+        };
+      }
+    ];
+  };
+}
